fb4a29d14df01498f3cb77a282d445c1ed8a58fe
Mhdsuhail5430 <mohamedsuhail5430@gmail.com>
2022-06-09 19:21:44 +0530

    Basics of java programs

---
+++ src/basics_Vers_1/VariableDemo.java
@@ -0,0 +1,47 @@
+package basics_Vers_1;
+
+public class VariableDemo {
+	public static void main(String[] args) {
+		//variable declaration
+		
+		int $i;
+		String s;
+		
+		
+		//variable initialization
+		$i=9;
+		s="stringgg";
+		float f=1.4f;
+		double d=1.44;
+		float fff=1.44f;
+		char c='c';
+		char cc=88;
+		boolean b=true;
+		boolean bbbbb=false;
+		
+		//valid identifiers or valid name
+
+		 int a;//valid identifier
+		 /*
+		 * 2age - invalid identifier - because the name starts with a
+		number
+		 * @age - invalid identifier - because the name starts with a
+		special character other than $ and _
+		 * $age - valid identifer
+		 * 
+		 * * _age - valid identifier
+ * a2ge - valid identifier - you can have numbers in between
+ * a2g_e$ - valid identifier
+ * ag e - invalid identifier - because there is a space
+ *
+ * YOU CANNOT CREATE A NAME WHICH IS A KEYWORD OR CLASSNAME IN
+JAVA
+ * The valid identifier concept applies to even the CLASS NAMES
+which u create.
+ */
+		 //183611585
+		
+
+	}
+	
+}
